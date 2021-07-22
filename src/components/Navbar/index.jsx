57a9c961cbf092f8a588641947f72afd46603ac2
Maenhout <victor@maenhout.fr>
2021-07-22 13:17:21 +0200

    Créaton du composent Navbar sans style

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+import SNavbar from './style';
+
+function Navbar() {
+  return (
+    <SNavbar>
+      <a href="/">Accueil</a>
+      <a href="/">Règles</a>
+      <a href="/">Aventure</a>
+    </SNavbar>
+  );
+}
+export default Navbar;
---
+++ src/components/Navbar/style.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const SNavbar = styled.div``;
+
+export default SNavbar;
